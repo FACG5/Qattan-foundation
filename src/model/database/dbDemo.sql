586218b74ee7e34394bf716ce5ec985464df3b54
AsalaKM <asalamatarya@gmail.com>
2018-10-01 18:06:09 +0300

    create database make connection write queries for ticket

---
+++ src/model/database/dbDemo.sql
@@ -0,0 +1,4 @@
+INSERT INTO department (department) VALUES ('Administration'),('qss'),('library & technical'),('information technology');
+INSERT INTO employee (name,password,department) VALUES ('asala','123','Administration'),('salwa','123','qss'),('ons','123','library & technical'),('mohammed','123','information technology');
+INSERT INTO problem_type (type) VALUES ('hardware'),('software'),('m2l'),('internet'),('lan'),('customer db');
+INSERT INTO status_type (status) VALUES ('solved'),('in progress'), ('not solved');
