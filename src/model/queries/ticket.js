a166ea876a8b330f1bd149e0cb331609266b9093
AsalaKM <asalamatarya@gmail.com>
2018-10-03 22:31:37 +0300

    Edit ticket.js for queries write all possible queries which related to ticket add, select, filter and update relates #2

--- src/model/queries/ticket.js
+++ src/model/queries/ticket.js
@@ -62,13 +62,51 @@ const getLoans = () => {
   return dbConnection.query(sql);
 };
 
+const getTicketByStatus = (status) => {
+  const sql = {
+    text: 'SELECT * FROM ticket Where status_type like $1',
+    values: [status],
+  };
+  return dbConnection.query(sql);
+};
+const getTicketByPeriod = (minPeriod, maxPeriod) => {
+  const sql = {
+    text: 'SELECT * FROM ticket Where ticket_date between $1 and $2 order by ticket_date asc',
+    values: [minPeriod, maxPeriod],
+  };
+  return dbConnection.query(sql);
+};
+
+const addTicket = (newTicket) => {
+  const {
+    employee, department, problemType, subject, duration, technicalDesc, type,
+  } = newTicket;
+  const sql = {
+    text: 'INSERT INTO ticket (employee,department,problem_type,subject,duration,technical_desc,type) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+    values: [employee, department, problemType, subject, duration, technicalDesc, type],
+  };
+  return dbConnection.query(sql);
+};
+
+const updateTicket = (itEmployee, status, ticketNo) => {
+  const sql = {
+    text: 'UPDATE ticket SET status_type = $1, it_employee = $2 WHERE ticket_no = $3;',
+    values: [itEmployee, status, ticketNo],
+  };
+  return dbConnection.query(sql);
+};
+
 module.exports = {
   getLatestTicket,
   getTicketByName,
-  getSolvedCount,
-  getTicketCount,
   getTicketDetails,
+  getTicketCount,
+  getSolvedCount,
   getNotSolvedCount,
   getSupports,
   getLoans,
+  getTicketByStatus,
+  getTicketByPeriod,
+  addTicket,
+  updateTicket,
 };
