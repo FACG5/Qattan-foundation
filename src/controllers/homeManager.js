0af854ba96b625fd637de4da9378ffd2d8b69d50
AsalaKM <asalamatarya@gmail.com>
2018-10-04 00:14:34 +0300

    managerHome Craete homeManager router Create homeManager.hbs View latest ticket View Count of ticket, solved ticket and not solved

---
+++ src/controllers/homeManager.js
@@ -0,0 +1,35 @@
+const {
+  getLatestTicket, getTicketCount, getNotSolvedCount, getSolvedCount,
+} = require('../model/queries/ticket');
+
+exports.get = (req, res, next) => {
+  getLatestTicket()
+    .then((response) => {
+      const result = (response.rows);
+      getTicketCount()
+        .then((countTicket) => {
+          const tCount = (countTicket.rows);
+          getSolvedCount()
+            .then((countSolved) => {
+              const tSolved = (countSolved.rows);
+              getNotSolvedCount()
+                .then((countNotSolved) => {
+                  const tNotSolved = (countNotSolved.rows);
+                  res.render('homeManager', {
+                    style: 'master',
+                    title: 'Manager',
+                    dom: 'homeManager',
+                    result,
+                    tCount,
+                    tSolved,
+                    tNotSolved,
+                  });
+                })
+                .catch(error => next(error));
+            })
+            .catch(error => next(error));
+        })
+        .catch(error => next(error));
+    })
+    .catch(error => next(error));
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const homeManager = require('./homeManager');
+
+router.get('/', homeManager.get);
+
 module.exports = router;
