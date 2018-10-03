9296cadd1e87d5331ca0c83c9c6f7a2c27739d1b
AsalaKM <asalamatarya@gmail.com>
2018-10-03 10:28:20 +0300

    db connection

--- src/model/queries/ticket.js
+++ src/model/queries/ticket.js
@@ -1,70 +1,43 @@
 const dbConnection = require('../database/dbConnection');
 
-const getLatestTicket = () => new Promise((resolve, reject) => {
+const getLatestTicket = () => {
   const sql = {
     text: 'SELECT * FROM ticket ORDER BY ticket_no DESC',
   };
-  dbConnection.query(sql, (error, res) => {
-    if (error) return reject(error);
-    return resolve(res.rows);
-  });
-});
+  return dbConnection.query(sql);
+};
 
-const getTicketByName = name => new Promise((resolve, reject) => {
+const getTicketByName = (name) => {
   const sql = {
     text: 'SELECT * FROM ticket Where employee = $1;',
     values: [name],
   };
-  dbConnection.query(sql, (err, res) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(res.rows);
-    }
-  });
-});
+  dbConnection.query(sql);
+};
 
-const getTicketDetails = id => new Promise((resolve, reject) => {
+const getTicketDetails = (id)  => {
   const sql = {
     text: 'SELECT * FROM ticket Where ticket_no = $1;',
     values: [id],
   };
-  dbConnection.query(sql, (err, res) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(res.rows);
-    }
-  });
-});
+  dbConnection.query(sql);
+};
 
-const getSupports = () => new Promise((resolve, reject) => {
+const getSupports = () => {
   const sql = {
     text: 'SELECT * FROM ticket Where type =$1',
     values: ['support'],
   };
-  dbConnection.query(sql, (err, res) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(res.rows);
-    }
-  });
-});
+  dbConnection.query(sql);
+};
 
-const getLoans = () => new Promise((resolve, reject) => {
+const getLoans = () => {
   const sql = {
     text: 'SELECT * FROM ticket Where type =$1',
     values: ['loan'],
   };
-  dbConnection.query(sql, (err, res) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(res.rows);
-    }
-  });
-});
+  dbConnection.query(sql);
+};
 
 module.exports = {
   getLatestTicket,
