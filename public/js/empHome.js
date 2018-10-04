38fae38f79ea04ca8943a162b2ae3092c284f299
Ons Nader <onsmariposa-nana@hotmail.com>
2018-10-04 08:57:16 +0300

    added all previous work

---
+++ public/js/empHome.js
@@ -0,0 +1,3 @@
+function move(x) {
+  x.classList.toggle('change');
+}
---
+++ src/model/database/db_Build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const dbConnection = require('./db_connection');
+
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
+
+const dbBuild = cb => dbConnection.query(sql, (err, res) => {
+  if (err) cb(err);
+  cb(null, res);
+});
+
+module.exports = dbBuild;
