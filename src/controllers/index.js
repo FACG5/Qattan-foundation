ec646457bb708176e14b4bc34bfbb2d2d29fefd5
AsalaKM <asalamatarya@gmail.com>
2018-10-04 10:22:22 +0300

    Craete homeManager router Create homeManager.hbs View Count for ticket, solved ticket and not solved View latest ticket #24

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+const homeManager = require('./homeManager');
+
+router.get('/', homeManager.get);
+
+
 module.exports = router;
