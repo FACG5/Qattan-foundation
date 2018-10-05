1cf70ccd34ed0fcd0a9cac659212264cd847d480
AsalaKM <asalamatarya@gmail.com>
2018-10-05 13:16:51 +0300

    Support page View all ticket with status type equal not solved or in progress. Relates #11

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 const homeManager = require('./homeManager');
 
+const supports = require('./support');
+
 router.get('/', homeManager.get);
 
+router.get('/support', supports.get);
 
 module.exports = router;
