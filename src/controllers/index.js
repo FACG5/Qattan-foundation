c8f42cd94dee2094c004f8f98f0f1b04bfa7ef02
AsalaKM <asalamatarya@gmail.com>
2018-10-08 02:53:02 +0300

    loans detail page > View all ticket detail. > Allow the user to update the ticket and add new status type, technical description and the IT employee name > Relates #14

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,5 +6,30 @@ const homeManager = require('./homeManager');
 
 router.get('/', homeManager.get);
 
+<<<<<<< Updated upstream
+=======
+const loans = require('./loans');
+
+const supportDetails = require('./supportDetails');
+
+const loanDetails = require('./loanDetails');
+
+router.route('/')
+  .get(homeManager.get);
+
+router.route('/support')
+  .get(supports.get);
+
+router.route('/loans')
+  .get(loans.get);
+
+router.route('/support/:id')
+  .get(supportDetails.get)
+  .put(supportDetails.put);
+>>>>>>> Stashed changes
+
+router.route('/loan/:id')
+  .get(loanDetails.get)
+  .put(loanDetails.put);
 
 module.exports = router;
