c5000b69a495423bbf0ce43dac9be9fb481fa18a
Ons <36223611+OnsNader@users.noreply.github.com>
2018-10-09 16:47:31 +0300

    Updated error pages

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -2,7 +2,6 @@ exports.clientError = (req, res) => {
   res.status(404).render('error', {
     layout: 'error',
     title: 'Error 404',
-    // style: ['error'],
     style_error: 'error',
     statusCode: '404',
     errorMsg: 'Page not found',
@@ -13,7 +12,6 @@ exports.serverError = (error, req, res) => {
   res.status(500).render('error', {
     layout: 'error',
     title: 'Error 500',
-    // style: ['error'],
     statusCode: '500',
     errorMsg: 'Internal Server Error',
   });
