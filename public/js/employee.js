80760eec4ab6347f6e18617a947da97500db61b9
AsalaKM <asalamatarya@gmail.com>
2018-10-10 14:20:28 +0300

    Employee page

--- public/js/employee.js
+++ public/js/employee.js
@@ -21,9 +21,13 @@ const unDoneSection = document.querySelector('#undone');
 unDoneBtn.addEventListener('click', () => {
   doneSection.style.display = 'none';
   unDoneSection.style.display = 'block';
+  unDoneBtn.classList.remove('tap');
+  doneBtn.classList.add('tap');
 });
 
 doneBtn.addEventListener('click', () => {
   unDoneSection.style.display = 'none';
   doneSection.style.display = 'block';
+  doneBtn.classList.remove('tap');
+  unDoneBtn.classList.add('tap');
 });
