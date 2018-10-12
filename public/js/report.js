7fa8b4f1a4c648d2c4f580911fb83c50a948e105
AsalaKM <asalamatarya@gmail.com>
2018-10-12 20:21:16 +0300

    Edit view and adding css style for large and small screen

--- public/js/report.js
+++ public/js/report.js
@@ -1,13 +1,25 @@
 /* global document fetch   */
 /* eslint-disable file no-param-reassign */
 
+const createElement = ((tagName, tagClassName, textContent) => {
+  const element = document.createElement(tagName);
+  element.classList.add(tagClassName);
+  element.textContent = (textContent);
+  return element;
+});
+
 
 const periodBtn = document.querySelector('#period-button');
 const statusBtn = document.querySelector('#status-button');
 const typeBtn = document.querySelector('#type-button');
+const allBtn = document.querySelector('#all-button');
 
+const all = document.querySelector('.all');
 const periodSection = document.querySelector('.period-section');
-const reportSection = document.querySelector('.filter');
+const reportSection = document.querySelector('.report');
+
+const statusSection = document.querySelector('.status-filter');
+const typeSection = document.querySelector('.type-filter');
 
 const switchType = document.querySelector('.switch-type');
 const switchStatus = document.querySelector('.switch-status');
@@ -18,14 +30,13 @@ const supportBtn = document.querySelector('#support-button');
 const loans = document.querySelectorAll('.loan');
 const supports = document.querySelectorAll('.support');
 
-
 const solvedBtn = document.querySelector('#solved-button');
 const notSolvedBtn = document.querySelector('#not-solved-button');
 const inProgressBtn = document.querySelector('#in-progress-button');
 
 const solved = document.querySelectorAll('.solved');
-const notSolved = document.querySelectorAll('.not');
-const inProgress = document.querySelectorAll('.in');
+const notSolved = document.querySelectorAll('.notSolved');
+const inProgress = document.querySelectorAll('.progress');
 
 periodSection.style.display = 'none';
 switchType.style.display = 'none';
@@ -37,20 +48,74 @@ periodBtn.addEventListener('click', () => {
   reportSection.style.display = 'none';
   switchType.style.display = 'none';
   switchStatus.style.display = 'none';
+  periodBtn.classList.add('selected');
+  statusBtn.classList.remove('selected');
+  typeBtn.classList.remove('selected');
+  allBtn.classList.remove('selected');
+});
+
+
+allBtn.addEventListener('click', () => {
+  periodSection.style.display = 'none';
+  reportSection.style.display = 'block';
+  all.style.display = 'block';
+  typeSection.style.display = 'none';
+  statusSection.style.display = 'none';
+  switchType.style.display = 'none';
+  switchStatus.style.display = 'none';
+  allBtn.classList.add('selected');
+  periodBtn.classList.remove('selected');
+  statusBtn.classList.remove('selected');
+  typeBtn.classList.remove('selected');
 });
 
 statusBtn.addEventListener('click', () => {
   periodSection.style.display = 'none';
+  reportSection.style.display = 'block';
+  all.style.display = 'none';
   switchStatus.style.display = 'block';
   switchType.style.display = 'none';
   switchStatus.style.display = 'block';
+  typeSection.style.display = 'none';
+  statusSection.style.display = 'block';
+  solved.forEach((element) => {
+    element.style.display = 'none';
+  });
+  notSolved.forEach((element) => {
+    element.style.display = 'none';
+  });
+  inProgress.forEach((element) => {
+    element.style.display = 'none';
+  });
+  statusBtn.classList.add('selected');
+  periodBtn.classList.remove('selected');
+  typeBtn.classList.remove('selected');
+  allBtn.classList.remove('selected');
+  solvedBtn.classList.remove('btnClicked');
+  inProgressBtn.classList.remove('btnClicked');
+  notSolvedBtn.classList.remove('btnClicked');
 });
 
 typeBtn.addEventListener('click', () => {
   periodSection.style.display = 'none';
-  reportSection.style.display = 'none';
+  reportSection.style.display = 'block';
+  all.style.display = 'none';
   switchStatus.style.display = 'none';
   switchType.style.display = 'block';
+  typeSection.style.display = 'block';
+  loans.forEach((element) => {
+    element.style.display = 'none';
+  });
+  supports.forEach((element) => {
+    element.style.display = 'none';
+  });
+  statusSection.style.display = 'none';
+  typeBtn.classList.add('selected');
+  statusBtn.classList.remove('selected');
+  periodBtn.classList.remove('selected');
+  allBtn.classList.remove('selected');
+  supportBtn.classList.remove('btnClicked');
+  loanBtn.classList.remove('btnClicked');
 });
 
 loanBtn.addEventListener('click', () => {
@@ -58,12 +123,16 @@ loanBtn.addEventListener('click', () => {
   switchStatus.style.display = 'none';
   switchType.style.display = 'block';
   reportSection.style.display = 'block';
+  typeSection.style.display = 'block';
+  statusSection.style.display = 'none';
   loans.forEach((element) => {
     element.style.display = 'block';
   });
   supports.forEach((element) => {
     element.style.display = 'none';
   });
+  loanBtn.classList.add('btnClicked');
+  supportBtn.classList.remove('btnClicked');
 });
 
 supportBtn.addEventListener('click', () => {
@@ -71,12 +140,16 @@ supportBtn.addEventListener('click', () => {
   switchStatus.style.display = 'none';
   switchType.style.display = 'block';
   reportSection.style.display = 'block';
+  typeSection.style.display = 'block';
+  statusSection.style.display = 'none';
   loans.forEach((element) => {
     element.style.display = 'none';
   });
   supports.forEach((element) => {
     element.style.display = 'block';
   });
+  supportBtn.classList.add('btnClicked');
+  loanBtn.classList.remove('btnClicked');
 });
 
 solvedBtn.addEventListener('click', () => {
@@ -84,6 +157,8 @@ solvedBtn.addEventListener('click', () => {
   switchStatus.style.display = 'block';
   switchType.style.display = 'none';
   reportSection.style.display = 'block';
+  typeSection.style.display = 'none';
+  statusSection.style.display = 'block';
   solved.forEach((element) => {
     element.style.display = 'block';
   });
@@ -93,6 +168,9 @@ solvedBtn.addEventListener('click', () => {
   inProgress.forEach((element) => {
     element.style.display = 'none';
   });
+  solvedBtn.classList.add('btnClicked');
+  inProgressBtn.classList.remove('btnClicked');
+  notSolvedBtn.classList.remove('btnClicked');
 });
 
 notSolvedBtn.addEventListener('click', () => {
@@ -100,6 +178,8 @@ notSolvedBtn.addEventListener('click', () => {
   switchStatus.style.display = 'block';
   switchType.style.display = 'none';
   reportSection.style.display = 'block';
+  typeSection.style.display = 'none';
+  statusSection.style.display = 'block';
   solved.forEach((element) => {
     element.style.display = 'none';
   });
@@ -109,6 +189,9 @@ notSolvedBtn.addEventListener('click', () => {
   inProgress.forEach((element) => {
     element.style.display = 'none';
   });
+  notSolvedBtn.classList.add('btnClicked');
+  inProgressBtn.classList.remove('btnClicked');
+  solvedBtn.classList.remove('btnClicked');
 });
 
 inProgressBtn.addEventListener('click', () => {
@@ -116,6 +199,8 @@ inProgressBtn.addEventListener('click', () => {
   switchStatus.style.display = 'block';
   switchType.style.display = 'none';
   reportSection.style.display = 'block';
+  typeSection.style.display = 'none';
+  statusSection.style.display = 'block';
   solved.forEach((element) => {
     element.style.display = 'none';
   });
@@ -125,21 +210,20 @@ inProgressBtn.addEventListener('click', () => {
   inProgress.forEach((element) => {
     element.style.display = 'block';
   });
+  inProgressBtn.classList.add('btnClicked');
+  notSolvedBtn.classList.remove('btnClicked');
+  solvedBtn.classList.remove('btnClicked');
 });
 
 
 const filterBtn = document.querySelector('#btn-period');
 const maxDate = document.querySelector('#max-date');
 const minDate = document.querySelector('#min-date');
-const periodResult = document.querySelector('.ticket-list');
+const periodResult = document.querySelector('.period-result');
+const list = document.querySelector('.ticket-list');
 const error = document.querySelector('.error');
 
-const createElement = ((tagName, tagClassName, textContent) => {
-  const element = document.createElement(tagName);
-  element.classList.add(tagClassName);
-  element.textContent = (textContent);
-  return element;
-});
+periodResult.style.display = 'none';
 
 filterBtn.addEventListener('click', () => {
   if (maxDate && minDate) {
@@ -168,23 +252,31 @@ filterBtn.addEventListener('click', () => {
           return response.map((element) => {
             error.textContent = '';
             periodResult.style.display = 'block';
+            const tablerow = document.createElement('tr');
             const myDate = element.ticket_date.split('T')[0];
-            const ticketNo = createElement('h4', 'ticket-no', element.ticket_no);
-            const date = createElement('h5', 'date', myDate);
-            const employee = createElement('h6', 'employee', element.employee);
-            const subject = createElement('h6', 'desc', element.subject);
-            const desc = createElement('h6', 'desc', element.technical_desc);
-            const status = createElement('h6', 'status', element.status_type);
-            const type = createElement('h6', 'type', element.type);
-            const problem = createElement('h6', 'problem', element.problem_type);
-            periodResult.appendChild(ticketNo);
-            periodResult.appendChild(date);
-            periodResult.appendChild(employee);
-            periodResult.appendChild(subject);
-            periodResult.appendChild(desc);
-            periodResult.appendChild(status);
-            periodResult.appendChild(type);
-            periodResult.appendChild(problem);
+
+            const ticketNo = createElement('td', 'ticket-no', element.ticket_no);
+            const date = createElement('td', 'date', myDate);
+            const employee = createElement('td', 'employee', element.employee);
+            const subject = createElement('td', 'desc', element.subject);
+            const desc = createElement('td', 'desc', element.technical_desc);
+            const status = createElement('td', 'status', element.status_type);
+            const type = createElement('td', 'type', element.type);
+            const problem = createElement('td', 'problem', element.problem_type);
+            const itEmployee = createElement('td', 'it_employee', element.it_employee);
+            const duration = createElement('td', 'duration', element.duration);
+
+            list.appendChild(tablerow);
+            tablerow.appendChild(ticketNo);
+            tablerow.appendChild(date);
+            tablerow.appendChild(employee);
+            tablerow.appendChild(subject);
+            tablerow.appendChild(desc);
+            tablerow.appendChild(status);
+            tablerow.appendChild(type);
+            tablerow.appendChild(problem);
+            tablerow.appendChild(itEmployee);
+            tablerow.appendChild(duration);
           });
         }
       })
--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -1,16 +1,29 @@
 const {
-  getAllTicket, getTicketByPeriod,
+  getAllTicket, getTicketByPeriod, getLoans, getSupports, getNotSolved, getTicketByStatus,
 } = require('../model/queries/ticket');
 
 exports.get = async (req, res, next) => {
   try {
+    const solved = 'solved';
+    const progress = 'in';
     const ticket = await getAllTicket();
+    const tLoans = await getLoans();
+    const tSupports = await getSupports();
+    const tSolve = await getTicketByStatus(solved);
+    const tNotSolved = await getNotSolved();
+    const tInProgress = await getTicketByStatus(progress);
     res.render('reports', {
       style: 'master',
+      style_special: 'report',
       title: 'تقارير',
       dom: 'report',
       manager: true,
       result: ticket.rows,
+      loans: tLoans.rows,
+      supports: tSupports.rows,
+      solve: tSolve.rows,
+      notSolved: tNotSolved.rows,
+      inProgress: tInProgress.rows,
     });
   } catch (err) {
     next(err);
