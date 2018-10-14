14a65dd7610c21ce30a2442b4af0bdcdf246244a
AsalaKM <asalamatarya@gmail.com>
2018-10-14 10:13:22 +0300

    add sweet alerts for update ticket page

--- public/js/updateTicket.js
+++ public/js/updateTicket.js
@@ -1,5 +1,7 @@
 /* global document fetch window */
 
+const img = 'http://qattanfoundation.org/sites/all/themes/qf/logo.svg';
+
 const updateBtn = document.getElementById('update-btn');
 const updateSection = document.querySelector('.update-section');
 const closeBtn = document.querySelector('#close-btn');
@@ -48,11 +50,34 @@ doneBtn.addEventListener('click', (e) => {
       body: JSON.stringify(data),
     })
       .then(response => response.json())
-      .then((response) => {
-        window.location = '/support';
-      })
-      .catch((err) => {
-        error.textContent = 'THERE IS ERROR';
+      if (response.error) {
+        swal({
+          dangerMode: true,
+          title: 'فشلت العملية',
+          text: response.error,
+          icon: img,
+          button: 'أعد المحاولة',
+        });
+      } else {
+        setTimeout(() => {
+          window.location = '/support';
+        }, 3000);
+        swal({
+          title: 'تمت العملية:',
+          text: response.result,
+          icon: img,
+          button: 'حسناً',
+        });
+      }
+    })
+    .catch(() => {
+      swal({
+        dangerMode: true,
+        title: 'فشلت العملية',
+        text: 'خطأ في اﻹدخال',
+        icon: img,
+        button: 'أعد المحاولة',
       });
+    });
   }
 });
